ad9e7c76870272de27201edf9c8e3399f4d7fea6
Diana <daria.blessed18@gmail.com>
2023-04-06 14:38:03 +0300

    Add book permissions

--- books/views.py
+++ books/views.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets
-
+from rest_framework.authentication import TokenAuthentication
 
 from books.models import Book
+from books.permissions import AdminWriteOnly
 from books.serializers import BookSerializer
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (AdminWriteOnly, )
