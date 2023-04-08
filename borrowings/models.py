baff19cea8e0469ea0732923bca5527c9b333e73
Diana <daria.blessed18@gmail.com>
2023-04-08 19:47:45 +0300

    Create borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,33 @@
+from datetime import datetime
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+from books.models import Book
+from user.models import User
+
+
+class Borrowing(models.Model):
+    borrow_date = models.DateTimeField(
+        validators=[MinValueValidator(limit_value=datetime.now)],
+        help_text='The date when the book was borrowed.'
+    )
+    expected_return_date = models.DateTimeField(
+        validators=[MinValueValidator(limit_value=datetime.now)],
+        help_text='The date when the book is expected to be returned.'
+    )
+    actual_return_date = models.DateTimeField(
+        validators=[MinValueValidator(limit_value=datetime.now)],
+        help_text='The date when the book was actually returned.'
+    )
+    book = models.ForeignKey(
+        Book,
+        related_name="borrowings",
+        null=True,
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        User,
+        related_name="borrowings",
+        on_delete=models.CASCADE
+    )
