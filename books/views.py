ba2dc3ab28be51f0d2933e70c3531559963f4d8d
Diana <daria.blessed18@gmail.com>
2023-04-10 13:57:10 +0300

    Create borrowing endpoints

--- books/views.py
+++ books/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from books.models import Book
 from books.permissions import AdminWriteOnly
--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from books.permissions import AdminWriteOnly
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingDetailSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    model = Borrowing
+    serializer_class = BorrowingSerializer
+    permission_classes = (AdminWriteOnly,)
+    queryset = Borrowing.objects.select_related("user", "book")
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+
+        return BorrowingSerializer
